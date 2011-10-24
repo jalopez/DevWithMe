8d62e06b85feed4c6c16dab66bdeaf12ca46d71c
Javier Lopez <jlopez@fi.upm.es>
2011-10-24 18:34:10 +0200

    Login mechanism

--- urls.py
+++ urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='auth_logout'),
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^tag', include('tag.urls')),
     #url(r'^publication', include('publication.urls')),
