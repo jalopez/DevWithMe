4e1b76c1dc6c6e8343f3681685ed719a498acedd
Juanjo Martin <jjmr83@gmail.com>
2011-10-24 18:46:33 +0200

    urls properly ordered

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('user_profile.views',
-    url(r'$', 'general_feed'),
-    url(r'(?P<username>\w+)$', 'user_feed')
+    url(r'(?P<username>\w+)$', 'user_feed'),
+    url(r'$', 'general_feed'),    
 )
