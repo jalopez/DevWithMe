71a49a58b3f8b101e6239dc9a979ef668b2fcf2b
Javier Lopez <jlopez@fi.upm.es>
2011-10-25 13:08:41 +0200

    Tag title

--- tag/views.py
+++ tag/views.py
@@ -6,4 +6,4 @@ from models import Tag
 @render_to('feed.html')
 def tag_feed(request, tag):
     tag_model = Tag.objects.get(tag=tag)
-    return {'feed': tag_model.get_feed()}
+    return {'feed': tag_model.get_feed(), 'tag_feed': tag}
